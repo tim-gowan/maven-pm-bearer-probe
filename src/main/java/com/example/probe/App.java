42323cac6188aa7c5a8374df922a099c7c7fbcbe
Tim Gowan <tim@example.com>
2026-09-09 10:24:50 -0500

    Depend on private Artifactory canary artifact for PM auth probe

--- src/main/java/com/example/probe/App.java
+++ src/main/java/com/example/probe/App.java
@@ -1,9 +1,7 @@
 package com.example.probe;
 
-import org.apache.commons.io.FilenameUtils;
-
 public class App {
   public static void main(String[] args) {
-    System.out.println(FilenameUtils.getExtension("probe.txt"));
+    System.out.println("probe");
   }
 }
